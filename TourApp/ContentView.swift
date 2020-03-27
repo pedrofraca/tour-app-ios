1c1e47fd4645df12c09cb8b39698c28aa9880433
Pedro Fraca <pedro.fraca@gmail.com>
2020-03-27 22:26:14 +0100

    Initial Commit

---
+++ TourApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TourApp
+//
+//  Created by Pedro Fraca Tarancon on 27.03.20.
+//  Copyright © 2020 Pedro Fraca Tarancon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
